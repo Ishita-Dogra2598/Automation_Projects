29388f5c5d06ab61a964daaff9fdf037aef9a693
ishitadogra98 <ishitadogra98@gmail.com>
2024-11-13 16:09:02 +0530

    ME_QA_XGOOGLE_FORM_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,34 +1,38 @@
 package demo;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+
+
+
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import demo.wrappers.Wrappers;
+
+
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
-import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
 
-     
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -42,18 +46,45 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
     }
 
     @AfterTest
-    public void endTest()
-    {
+    public void endTest() {
         driver.close();
         driver.quit();
+
+    }
+
+    @Test
+    public void testCase01() {
+        Wrappers wrap=new Wrappers(driver) ;
+        //Open the website
+        wrap.openUrl("https://docs.google.com/forms/d/e/1FAIpQLSep9LTMntH5YqIXa5nkiPKSs283kdwitBBhXWyZdAS-e4CxBQ/viewform");
+        //wait for the page to load 
+        wrap.implicitWait();
+        //1st Question  ---- Name
+        wrap.NameQuestion("Crio Learner");
+        //2nd Question ----Why are you practicing Automation?
+        wrap.practicingAutomationQuestion("I want to be the best QA Engineer!");
+        //3rd Question ---How much experience do you have in Automation Testing?
+        wrap.ExpirenceinAutomationQuestion("0 - 2");
+        //4th Question -----Which of the following have you learned in Crio.Do for Automation Testing?
+        wrap.ToolsLearnedQuestion("Java", "Selenium", "TestNG");
+        //5th Question ----How should you be addressed?
+          wrap.SalutationQuestion();
+        //6th Question ----What was the date 7 days ago?
+        wrap.PreviousDateQuestion();
+        //7th Question ----What is the time right now?
+        wrap.TimeQuestion("7", "30");
+        //Submit Button
+        wrap.SubmitButton();
+        //Success Message Check
 
     }
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,19 +1,172 @@
 package demo.wrappers;
 
-import java.time.Instant;
-import java.time.LocalDateTime;
+
+import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.List;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+
+
 import java.time.Duration;
 
-public class Wrappers {
+public class Wrappers  {
     /*
      * Write your selenium wrappers here
      */
+      ChromeDriver driver;
+      WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+      public Wrappers( ChromeDriver driver)
+      {
+         this.driver=driver;
+      }
+      public void openUrl(String url)
+
+      {
+        driver.get(url);
+      }
+      
+      public void  implicitWait()
+      {
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+      }
+
+      private  static void clickaction(ChromeDriver driver,WebElement element)
+      {
+        JavascriptExecutor js= (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].click();",element);
+      }
+
+
+      private void enterinput(String input ,WebElement element)
+      {
+           element.sendKeys(input);
+      }
+
+
+      private String  DateChanger()
+      {
+        LocalDate currDate=LocalDate.now();
+        LocalDate minus7days=currDate.minusDays(7);
+        DateTimeFormatter formatter=DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formatDate=minus7days.format(formatter);
+        return formatDate;
+      }
+
+      public void NameQuestion( String Answer)
+      {
+        try {
+          WebElement NameTextbox = driver.findElement(By.xpath("//div[text()='Your answer']//preceding-sibling::input"));
+          Thread.sleep(1000);
+          enterinput(Answer, NameTextbox);
+          
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+       
+      }
+
+      public void practicingAutomationQuestion(String Answer)
+      {
+        try{
+        long epoch = System.currentTimeMillis() / 1000;
+        String EpochTime = String.valueOf(epoch);
+        WebElement PracticeAutomationTextbox = driver.findElement(By.xpath("//textarea[@aria-label='Your answer']"));
+        Thread.sleep(1000);
+        enterinput(Answer+ EpochTime, PracticeAutomationTextbox);
+        }
+        catch (Exception e)
+        {
+          System.out.println(e);
+        }
+      }
+
+      public void ExpirenceinAutomationQuestion(String option)
+      {
+        try{
+        WebElement Radiot=driver.findElement(By.xpath("//div[@data-value='"+option+"']"));
+        Thread.sleep(2000);
+        clickaction(driver, Radiot);
+        }
+        catch(Exception e){
+          System.out.println(e);
+        }
+      }
+
+      public void ToolsLearnedQuestion(String option1,String option2,String option3)
+      {
+        String[] arr={option1,option2,option3};
+        for(int i=0;i<3;i++)
+        {
+          
+          WebElement ToolBox=driver.findElement(By.xpath("//div[@data-answer-value='"+arr[i]+"']"))   ; 
+          clickaction(driver, ToolBox);
+        }
+      }
+
+            
+
+
+      public void SalutationQuestion()
+      {
+        try {
+          WebElement ParentDropdownLocator = driver.findElement(By.xpath("//div[contains(@class,'DEh1R')]"));
+          clickaction(driver, ParentDropdownLocator);
+          Thread.sleep(1000);
+          WebElement DropdownOptions = driver.findElement(By.xpath("//div[@role='option' and @data-value='Ms']"));
+          clickaction(driver, DropdownOptions);
+           Thread.sleep(1000);
+          
+        } catch (Exception e) {
+          System.out.println(e);
+        } 
+      
+      }
+
+      public void PreviousDateQuestion()
+      {
+
+        WebElement Prevdate=driver.findElement(By.xpath("//input[@max='2074-01-01']"));     
+        enterinput(DateChanger(), Prevdate);
+      }
+
+
+      public void TimeQuestion(String Hour,String Minutes)
+      {
+        WebElement HourText = driver.findElement(By.xpath("//input[@aria-label='Hour']"));
+        wait.until(ExpectedConditions.visibilityOf(HourText));
+        HourText.sendKeys(Hour);
+        WebElement MinText = driver.findElement(By.xpath("//input[@aria-label='Minute']"));
+        wait.until(ExpectedConditions.visibilityOf(MinText));
+        MinText.sendKeys(Minutes);
+      }
+
+      public void SubmitButton()
+      {
+        try {
+          WebElement Submit=driver.findElement(By.xpath("//div[@aria-label='Submit']"));
+        Thread.sleep(3000);
+        clickaction(driver,Submit);
+        WebElement Message=driver.findElement(By.xpath("//div[@class='vHW8K']"));
+        Thread.sleep(3000);
+        System.out.println(Message.getText());
+          
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+        
+      }
+
+
+
 }
